6f7946a16734c5505473349d4b4f97731c29f10e
Avinash Sinha <avinashsinha751@gmail.com>
2026-04-09 23:58:26 +0600

    implemented routes index.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import { toNodeHandler } from "better-auth/node";
+import { auth } from './app/lib/auth';
+import router from './app/routes';
+import globalErrorHandler from './app/middleware/globalErrorHandler';
+import notFound from './app/middleware/notFound';
 
 const app: Application = express();
 
@@ -15,6 +20,12 @@ app.use(cors({
   credentials: true
 }));
 
+// Better-Auth Core Handler
+app.all("/api/v1/auth/*", toNodeHandler(auth));
+
+// Application Routes
+app.use('/api/v1', router);
+
 // Base Route
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
@@ -22,5 +33,9 @@ app.get('/', (req: Request, res: Response) => {
     message: 'Welcome to FoodHub API!'
   });
 });
+
+// Global Error Handler & Not Found Middleware
+app.use(notFound);
+app.use(globalErrorHandler);
 
 export default app;
---
+++ src/app/routes/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const router = express.Router();
+
+const moduleRoutes: any[] = [
+
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
